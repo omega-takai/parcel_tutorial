6a151612a2534afe9d5e80dfe26e5264f0da226a
tomonari takai <takai01010100@gmail.com>
2017-12-19 11:03:30 +0900

    :recycles: import from subdir

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 // import a CSS module
-import classes from './style/app.sass';
+import classes from './style/main.css';
+// import classes from './style/app.sass';
 
 export default () => {
-  console.log(classes.main);
+  console.log(classes.app);
 };
+
+// import classes from './main.css';
+
+// export default () => {
+//   console.log(classes.main);
+// };
