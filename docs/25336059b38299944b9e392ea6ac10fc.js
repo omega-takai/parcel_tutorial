31a645e526692e57a92373b8f9e6c1f31a2c3925
tomonari takai <takai01010100@gmail.com>
2018-01-04 19:44:41 +0900

    :gem: switch to CDN import

--- docs/25336059b38299944b9e392ea6ac10fc.js
+++ docs/25336059b38299944b9e392ea6ac10fc.js
@@ -65,10 +65,10 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({4:[function(require,module,exports) {
+})({6:[function(require,module,exports) {
 
-},{"./../../node_modules/yakuhanjp/dist/css/yakuhanjp.min.css":["61f29dfe5d10c2d41e68d8b4b24bd8cc.css",5]}],3:[function(require,module,exports) {
+},{}],5:[function(require,module,exports) {
 "use strict";function e(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(exports,"__esModule",{value:!0});var t=require("./style/app.scss"),r=e(t);exports.default=function(){};
-},{"./style/app.scss":4}],2:[function(require,module,exports) {
+},{"./style/app.scss":6}],4:[function(require,module,exports) {
 "use strict";function e(e){return e&&e.__esModule?e:{default:e}}var u=require("./main"),r=e(u);(0,r.default)();
-},{"./main":3}]},{},[2])
+},{"./main":5}]},{},[4])
